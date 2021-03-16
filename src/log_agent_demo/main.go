ab633a0fb8053213d608787914647410c29a1852
sheep-yang <58797353+sheep-yang@users.noreply.github.com>
2021-03-16 02:42:12 +0000

    Update main.go

--- src/log_agent_demo/main.go
+++ src/log_agent_demo/main.go
@@ -16,27 +16,19 @@ var Wg sync.WaitGroup
 
 func main() {
 	//1.初始化配置文件
-	logconf := new(conf.LogConf)
-	err := ini.MapTo(logconf, "./conf/conf.ini")
-	if err != nil {
-		fmt.Println("failed to load ini,err:", err)
-	}
+	
 	//2.动态从etcd获取配置信息
-	//etcd.Init(logconf.Etcdinfo.Etcdipaddr)
+	
 
 	//3.初始化kafaka连接
-	kafaka.Init(logconf.Kafaka.Ipaddr, logconf.Kafaka.Topic)
+	
 
 	//4.初始化logtail
-	logtailf.Initlog(logconf.Path)
+	
 	//5.初始化es
-	es.Init(logconf.Esinfo.Esurl)
+	
 	//6.发送消息到kafaka
-	Wg.Add(1)
-	go logtailf.Readlog()
-	go kafaka.SendMsg()
-	go kafaka.KafakaToEs()
-	Wg.Wait()
+	
 	//7.读取kafaka的消息，并写入es
 
 }
